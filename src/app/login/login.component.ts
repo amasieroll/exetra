01ebcc40773468bd0799517f10cb2e41a6e09468
x_almasier <a.masiero@lunalabs.it>
2022-04-28 17:52:08 +0200

    first-commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = this.fb.group({
+    user: ['', Validators.required],
+    password: ['', Validators.required]
+  
+  });
+
+
+  constructor(private fb: FormBuilder) { }
+
+ 
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.loginForm.value);
+  }
+  ngOnInit(): void {
+  }
+
+
+
+}
